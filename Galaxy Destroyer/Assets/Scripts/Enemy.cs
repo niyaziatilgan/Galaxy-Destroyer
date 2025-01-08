3ade9ef0e92d39f09f453152af0d5d01e342e1ad
Niyazi <niyaziatilgann@gmail.com>
2025-01-08 13:21:17 +0300

    Enemy Hit Points

--- Galaxy Destroyer/Assets/Scripts/Enemy.cs
+++ Galaxy Destroyer/Assets/Scripts/Enemy.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
     [SerializeField] GameObject destroyedVFX;
+    [SerializeField] int hitPoints = 3;
+
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedVFX, transform.position,Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+
+        if (hitPoints <= 0)
+        {
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
