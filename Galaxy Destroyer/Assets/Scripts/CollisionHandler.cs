0e8be3bbb81d159b57f7ac023335837c8fbc0fdb
Niyazi <niyaziatilgann@gmail.com>
2025-01-09 13:49:40 +0300

    Post Processing

--- Galaxy Destroyer/Assets/Scripts/CollisionHandler.cs
+++ Galaxy Destroyer/Assets/Scripts/CollisionHandler.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
+    GameSceneManager gameSceneManager;
 
- 
+    private void Start()
+    {
+        gameSceneManager = FindFirstObjectByType<GameSceneManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        gameSceneManager.ReloadLevel();
         Instantiate(destroyedVFX, transform.position, Quaternion.identity);
 
         //Debug.Log("Hit" + other.name);
